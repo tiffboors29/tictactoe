bc75bf45f662ca9b63060e79e8e3aeed7bb53a4b
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-01 20:45:40 -0400

    Get winner function created for rows/cols/diag of x/o
    Cat's game scenario has not yet been written. Only winning scenarios have been created

--- tictactoe.js
+++ tictactoe.js
@@ -39,3 +39,35 @@ var getPlayer2Move = function(move) {
   return move || getInput();
 };
 
+var getWinner = function(player1Move, player2Move) {
+  var winner;
+  if (r1t1 === r1t2 === r1t3 === 'o' ||
+      r1t1 === r2t2 === r3t3 === 'o' ||
+      r1t1 === r2t1 === r3t1 === 'o' ||
+      r1t2 === r2t2 === r3t2 === 'o' ||
+      r1t3 === r2t2 === r3t1 === 'o' ||
+      r1t3 === r2t3 === r3t3 === 'o' ||
+      r2t1 === r2t2 === r2t3 === 'o' ||
+      r3t1 === r3t2 === r3t3 === 'o') {
+    if (player1 === 'o') {
+      winner = player1;
+    } else {  // else 'o' must be player2
+      winner = player2;
+    }
+  if (r1t1 === r1t2 === r1t3 === 'x' ||
+      r1t1 === r2t2 === r3t3 === 'x' ||
+      r1t1 === r2t1 === r3t1 === 'x' ||
+      r1t2 === r2t2 === r3t2 === 'x' ||
+      r1t3 === r2t2 === r3t1 === 'x' ||
+      r1t3 === r2t3 === r3t3 === 'x' ||
+      r2t1 === r2t2 === r2t3 === 'x' ||
+      r3t1 === r3t2 === r3t3 === 'x') {
+    if (player1 === 'x') {
+      winner = player1;
+    } else {  // else 'x' must be player2
+      winner = player2;
+    }
+  }
+  // need to write cat's game scenario
+  return winner;
+};
