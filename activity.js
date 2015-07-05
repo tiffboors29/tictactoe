c5aa1673fdc3e0caa1d81032b52558119dc197b2
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-05 16:38:19 -0400

    Prompt working in activity to input player names. Tiles able to be changed with x or o
    Still working on game logic and incorporating it with activity.js

--- activity.js
+++ activity.js
@@ -1,52 +1,20 @@
 'use strict';
 
 
+
 // prompts players for names and places names in boxes on l-sidebar
 jQuery(document).ready(function() {
   var player1Name = prompt('Player 1, what is your name?');
   $('#player1-name').text(player1Name);
   var player2Name = prompt('Player2, what is your name?');
   $('#player2-name').text(player2Name);
-  newGame();
-});
-
-
-
-jQuery(document).ready(function() {
-  $('#tileA').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-  });
-  $('#tileB').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileB = 'x';
-  });
-  $('#tileC').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileC = 'x';
-  });
-  $('#tileD').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileD = 'x';
-  });
-  $('#tileE').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileE = 'x';
-  });
-  $('#tileF').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileF = 'x';
-  });
-  $('#tileG').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileG = 'x';
-  });
-  $('#tileH').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileH = 'x';
-  });
-  $('#tileI').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileI = 'x';
+  $('.default-tile').on('click', function() {
+    $(this).addClass();
   });
 });
+
+$('#tileA').on('click', function() {
+  $(this).addClass('o-layer');
+});
+
 
--- game-logic.js
+++ game-logic.js
@@ -1,184 +1,175 @@
 'use strict';
 
-var player1 = 'x';  // turn 0
-var player2 = 'o';  // turn 1
-var turn = 0;
-
-var tileA;
-var tileB;
-var tileC;
-var tileD;
-var tileE;
-var tileF;
-var tileG;
-var tileH;
-var tileI;
 
 
-
-// places 'x' or 'o' in box when clicked
-var newGame = function() {
-  if (turn == 0, function(event) {
-    event.preventDefault();
-    console.log("Player 1, click on the square you want to play.");
-    player1Move();
-    getWin();
-    turn == 1;
-    player2Move();
-    getWin();
-    }
-  });
-};
+// var player1;
+// var player2;
+// var turn;
+// var tileA, tileB, tileC, tileD, tileE, tileF, tileG, tileH, tileI;
 
 
-var playGame = function() {
-  while ( (tileA !== 'x' || 'o') &&
-          (tileB !== 'x' || 'o') &&
-          (tileC !== 'x' || 'o') &&
-          (tileD !== 'x' || 'o') &&
-          (tileE !== 'x' || 'o') &&
-          (tileF !== 'x' || 'o') &&
-          (tileG !== 'x' || 'o') &&
-          (tileH !== 'x' || 'o') &&
-          (tileI !== 'x' || 'o')  ) {
-    newGame();
-  }
-};
+// var setTurn = function() {  // randomly selects player 1 or 2 to go first
+//   var r = Math.random();
+//   if (r > 0.5) {
+//     turn = player1;
+//   } else {
+//     turn = player2;
+//   }
+//   return turn;
+// };
 
-var player1Move = function() {    // changes tile class to x-layer
-  $('#tileA').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileA = 'x';
-  });
-  $('#tileB').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileB = 'x';
-  });
-  $('#tileC').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileC = 'x';
-  });
-  $('#tileD').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileD = 'x';
-  });
-  $('#tileE').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileE = 'x';
-  });
-  $('#tileF').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileF = 'x';
-  });
-  $('#tileG').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileG = 'x';
-  });
-  $('#tileH').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileH = 'x';
-  });
-  $('#tileI').on('click', function() {
-    $(this).removeClass('default-tile').addClass('x-layer');
-    tileI = 'x';
-  });
-};
 
-var player2Move = function() {  // changes tile class to o-layer
-    $('#tileA').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileA = 'o';
-  });
-  $('#tileB').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileB = 'o';
-  });
-  $('#tileC').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileC = 'o';
-  });
-  $('#tileD').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileD = 'o';
-  });
-  $('#tileE').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileE = 'o';
-  });
-  $('#tileF').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileF = 'o';
-  });
-  $('#tileG').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileG = 'o';
-  });
-  $('#tileH').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileH = 'o';
-  });
-  $('#tileI').on('click', function() {
-    $(this).removeClass('default-tile').addClass('o-layer');
-    tileI = 'o';
-  });
-};
+// var playGame = function() {
+//   while ( ($('#tileA').html !== 'x' || 'o') &&
+//           ($('#tileB').html !== 'x' || 'o') &&
+//           ($('#tileC').html !== 'x' || 'o') &&
+//           ($('#tileD').html !== 'x' || 'o') &&
+//           ($('#tileE').html !== 'x' || 'o') &&
+//           ($('#tileF').html !== 'x' || 'o') &&
+//           ($('#tileG').html !== 'x' || 'o') &&
+//           ($('#tileH').html !== 'x' || 'o') &&
+//           ($('#tileI').html !== 'x' || 'o')  ) {
+//     setTurn();
+//     if (turn === player1) {
+//       getPlayer1Move();
+//     } else {
+//       getPlayer2Move();
+//   }
+//   }
+// };
+
+
+// var getPlayer1Move = function() {
+//   $('#alertBox').html('Player 1, click on the square you want to play.');
+
+//   if ( $('#tileA').click) {
+//     $('#tileA').addClass('x-layer');
+//     tileA = 'x';
+//   } else if ($('#tileB').click) {
+//     $('#tileB').addClass('x-layer');
+//     tileB = 'x';
+//   } else if ($('#tileC').click) {
+//     $('#tileC').addClass('x-layer');
+//     tileC = 'x';
+//   } else if ($('#tileD').click) {
+//     $('#tileD').addClass('x-layer');
+//     tileD = 'x';
+//   } else if ($('#tileE').click) {
+//     $('#tileE').addClass('x-layer');
+//     tileE = 'x';
+//   } else if ($('#tileF').click) {
+//     $('#tileF').addClass('x-layer');
+//     tileF = 'x';
+//   } else if ($('#tileG').click) {
+//     $('#tileG').addClass('x-layer');
+//     tileG = 'x';
+//   } else if ($('#tileH').click) {
+//     $('#tileH').addClass('x-layer');
+//     tileH = 'x';
+//   } else if ($('#tileI').click) {
+//     $('#tileI').addClass('x-layer');
+//     tileI = 'x';
+//   }
+// };
+
+// var getPlayer2Move = function() {
+//   $('#alertBox').html('Player 2, click on the square you want to play.');
+
+//   if ( $('#tileA').click) {
+//     $('#tileA').addClass('o-layer');
+//   } else if ($('#tileB').click) {
+//     $('#tileB').addClass('o-layer');
+//   } else if ($('#tileC').click) {
+//     $('#tileC').addClass('o-layer');
+//   } else if ($('#tileD').click) {
+//     $('#tileD').addClass('o-layer');
+//   } else if ($('#tileE').click) {
+//     $('#tileE').addClass('o-layer');
+//   } else if ($('#tileF').click) {
+//     $('#tileF').addClass('o-layer');
+//   } else if ($('#tileG').click) {
+//     $('#tileG').addClass('o-layer');
+//   } else if ($('#tileH').click) {
+//     $('#tileH').addClass('o-layer');
+//   } else if ($('#tileI').click) {
+//     $('#tileI').addClass('o-layer');
+//   }
+// };
 
 
 
-// detects win or cat's game
-var getWin = function() {
-  var xWins;
-  var oWins;
-  if (tileA === tileB === tileC === 'x' ||  // first row
-      tileD === tileE === tileF === 'x' ||  // second row
-      tileG === tileH === tileI === 'x' ||  // third row
-      tileA === tileD === tileG === 'x' ||  // first column
-      tileB === tileE === tileH === 'x' ||  // second column
-      tileC === tileF === tileI === 'x' ||  // last column
-      tileA === tileE === tileI === 'x' ||  // diagnal R-L
-      tileG === tileE === tileC === 'x' ||) { //diagnal L-R
-    xWin += 1;
-    console.log("Player 1 wins!");
-    clearBoard();
-    } else if (tileA === tileB === tileC === 'o' ||  // first row
-      tileD === tileE === tileF === 'o' ||  // second row
-      tileG === tileH === tileI === 'o' ||  // third row
-      tileA === tileD === tileG === 'o' ||  // first column
-      tileB === tileE === tileH === 'o' ||  // second column
-      tileC === tileF === tileI === 'o' ||  // last column
-      tileA === tileE === tileI === 'o' ||  // diagnal R-L
-      tileG === tileE === tileC === 'o' ||) { //diagnal L-R
-    oWin += 1;
-    console.log("Player 2 wins!");
-    clearBoard();
-    } else if (a !== null && b !== null && c !== null && // if all tiles are filled
-      d !== null && e !== null && f !== null && // but there are no winners
-      g !== null && h !== null && i !== null) { // it's a cat's game
-    console.log("Cat\'s Game. No Winners!")
-  }
-};
 
-// sets tile values back to empty strings
-var clearBoard = function() {
-  tileA = "";
-  tileB = "";
-  tileC = "";
-  tileD = "";
-  tileE = "";
-  tileF = "";
-  tileG = "";
-  tileH = "";
-  tileI = "";
-  xWin = 0;
-  oWin = 0;
-  newGame();
-};
 
-$(document).ready(function() {
-  var player1Name = prompt('Player 1, what is your name?');
-  $('#player1-name').html(player1Name);
-  var player2Name = prompt('Player2, what is your name?');
-  $('#player2-name').html(player2Name);
-  newGame();
-});
+
+// // sets tile values back to empty strings
+// var clearBoard = function() {
+//   tileA = "";
+//   tileB = "";
+//   tileC = "";
+//   tileD = "";
+//   tileE = "";
+//   tileF = "";
+//   tileG = "";
+//   tileH = "";
+//   tileI = "";
+//   xWin = 0;
+//   oWin = 0;
+//   newGame();
+// };
+
+
+
+
+
+// var getWin = function() {
+//   var xWins;
+//   var oWins;
+//   if (tileA === tileB === tileC === 'x' ||  // first row
+//       tileD === tileE === tileF === 'x' ||  // second row
+//       tileG === tileH === tileI === 'x' ||  // third row
+//       tileA === tileD === tileG === 'x' ||  // first column
+//       tileB === tileE === tileH === 'x' ||  // second column
+//       tileC === tileF === tileI === 'x' ||  // last column
+//       tileA === tileE === tileI === 'x' ||  // diagnal R-L
+//       tileG === tileE === tileC === 'x' ||) { //diagnal L-R
+//     xWin += 1;
+//     console.log("Player 1 wins!");
+//     clearBoard();
+//     } else if (tileA === tileB === tileC === 'o' ||  // first row
+//       tileD === tileE === tileF === 'o' ||  // second row
+//       tileG === tileH === tileI === 'o' ||  // third row
+//       tileA === tileD === tileG === 'o' ||  // first column
+//       tileB === tileE === tileH === 'o' ||  // second column
+//       tileC === tileF === tileI === 'o' ||  // last column
+//       tileA === tileE === tileI === 'o' ||  // diagnal R-L
+//       tileG === tileE === tileC === 'o' ||) { //diagnal L-R
+//     oWin += 1;
+//     console.log("Player 2 wins!");
+//     clearBoard();
+//     } else if (a !== null && b !== null && c !== null && // if all tiles are filled
+//       d !== null && e !== null && f !== null && // but there are no winners
+//       g !== null && h !== null && i !== null) { // it's a cat's game
+//     console.log("Cat\'s Game. No Winners!")
+//   }
+// };
+
+
+
+// // sets tile values back to empty strings
+// var clearBoard = function() {
+//   tileA = "";
+//   tileB = "";
+//   tileC = "";
+//   tileD = "";
+//   tileE = "";
+//   tileF = "";
+//   tileG = "";
+//   tileH = "";
+//   tileI = "";
+//   xWin = 0;
+//   oWin = 0;
+//   newGame();
+// };
+
+
 
