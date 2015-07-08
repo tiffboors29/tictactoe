1d432b42137a816e3bf45061303509d411b55e58
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-08 15:29:01 -0400

    Game working for new players and same players
    Wins counted and saved for multiple games played by same users

--- tictactoe.js
+++ tictactoe.js
@@ -32,24 +32,46 @@ var isAvailable = function(tile) {
   }
 };
 
-
-var player1NameWins = 0;    // trying to store
-var player2NameWins = 0;    // ''
+// variables store wins to be displayed in scoreBox when same users
+// play multiple games
+var player1NameWins = 0;
+var player2NameWins = 0;
 
 
 $(document).ready(function() {
 
+  // prompts for player names to be inputed in l-sidebar
+  var player1Name = prompt('Player 1, what is your name?');
+  $('#player1-name').text(player1Name);
+  var player2Name = prompt('Player2, what is your name?');
+  $('#player2-name').text(player2Name);
+  var x = setTurn(player1Name, player2Name);
 
-  // to Player Same Screen
+  displayAlert(x + ', You will be X. Please pick a square');
+
+  // sets 'count' (turn count) at zero and the score of each player at 0
+  var count = 0;
+  var player1NameWins = 0;
+  var player2NameWins = 0;
+
+
+  // reloads page for brand new game with new/different players
+  $('#newGame').on('click', function() {
+    window.location.reload(true);
+  });
+
+
+  // clears board so same players can play again and scores are saved
   $('#playAgain').on('click', function() {
 
+   // clears board to play again
    $('.default-tile').removeClass('o-layer');
    $('.default-tile').removeClass('x-layer');
 
+    // randomly set x (the player who goes first)
     var x = setTurn(player1Name, player2Name);
     displayAlert(x + ', You will be X. Please pick a square');
     var count = 0;
-
 
     // checks tiles to determine if there's a winner
     var checkWinner = function(layer, player1, player2) {
@@ -66,17 +88,16 @@ $(document).ready(function() {
         } else {
           player2NameWins++;
         }
+        displayScore(player1Name + ': ' + player1NameWins + ' ' + player2Name + ': ' + player2NameWins);
         $('.default-tile').off('click');
         displayAlert(player1 + ', You WON! Click "Play Again" to play again or "New Game" to restart');
-        displayScore(player1Name + ': ' + player1NameWins + ' ' + player2Name + ': ' + player2NameWins);
       } else if ( count === 9 ) {
           $('.default-tile').off('click');
-        displayAlert('Sorry '+ player1 + ' & ' + player2 + ', Cat\'s Game! Click "Play Again" to play again or "New Game" to restart');
+          displayAlert('Sorry '+ player1 + ' & ' + player2 + ', Cat\'s Game! Click "Play Again" to play again or "New Game" to restart');
       } else if (count < 9) {
         displayAlert(player2 + ', pick a square');
       }
     };
-
 
     // places x/o layers on clicked tiles
     $('.default-tile').on('click', function() {
@@ -118,20 +139,9 @@ $(document).ready(function() {
         }
       }
     });
-  }); // end of same player on click
+  });
 
-  // prompts for player names to be inputed in l-sidebar
-    var player1Name = prompt('Player 1, what is your name?');
-    $('#player1-name').text(player1Name);
-    var player2Name = prompt('Player2, what is your name?');
-    $('#player2-name').text(player2Name);
 
-    var x = setTurn(player1Name, player2Name);
-    displayAlert(x + ', You will be X. Please pick a square');
-    var count = 0;
-
-    var player1NameWins = 0;    // trying to add to [] for ss.storage.score
-    var player2NameWins = 0;    // ''
 
     // checks tiles to determine if there's a winner
     var checkWinner = function(layer, player1, player2) {
@@ -201,63 +211,6 @@ $(document).ready(function() {
       }
     });
 
-
-
-  // // places x/o layers on clicked tiles
-  // $('.default-tile').on('click', function() {
-  //   if (count === 0 || (count % 2 === 0)) {
-  //     if (x === player1Name) {
-  //       if (isAvailable($(this)) === false) {
-  //         displayAlert(player1Name + ', that square is taken. Choose another.');
-  //       } else {
-  //         $(this).addClass('x-layer');
-  //         count++;
-  //         checkWinner('x-layer', player1Name, player2Name);
-  //       }
-  //     } else if (x === player2Name) {
-  //       if (isAvailable($(this)) === false) {
-  //         displayAlert(player2Name + ', that square is taken. Choose another.');
-  //       } else {
-  //         $(this).addClass('x-layer');
-  //         count++;
-  //         checkWinner('x-layer', player2Name, player1Name);
-  //       }
-  //     }
-  //   } else {
-  //     if (x === player1Name) {
-  //       if (isAvailable($(this)) === false) {
-  //         displayAlert(player2Name + ', that square is taken. Choose another.');
-  //       } else {
-  //         $(this).addClass('o-layer');
-  //         count++;
-  //         checkWinner('o-layer', player2Name, player1Name);
-  //       }
-  //     } else if (x === player2Name) {
-  //       if (isAvailable($(this)) === false) {
-  //         displayAlert(player1Name + ', that square is taken. Choose another.');
-  //       } else {
-  //         $(this).addClass('o-layer');
-  //         count++;
-  //         checkWinner('o-layer', player1Name, player2Name);
-  //       }
-  //     }
-  //   }
-  // });
-
-  // reloads page for brand new game with new/different players
-  $('#newGame').on('click', function() {
-    window.location.reload(true);
-  });
-
-
-  // checks if tile is available
-  var isAvailable = function(tile) {
-    if (tile.hasClass('x-layer') || tile.hasClass('o-layer')) {
-      return false;
-    } else {
-      return true;
-    }
-  };
 
 
 });
