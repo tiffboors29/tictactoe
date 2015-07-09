498e7409a85f8d24d1d69572da034c518b8a6193
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-09 16:17:25 -0400

    Game Logic file updated so it is no longer dependent on HTML elements
    Game logic can run on its own, independent of html

--- game-logic.js
+++ game-logic.js
@@ -1,21 +1,33 @@
 'use strict';
 
+
+var board = ['', '', '', '', '', '', '', '', ''];
+var count = 0;
+
+var getInput = function() {
+  console.log('Please choose a square.');
+  return prompt();
+};
+
+
 // randomly selects player to be 'x' and go first
 var setTurn = function(player1Name, player2Name) {
   var x;
   var o;
   if (Math.random() < 0.5) {
     x = player1Name;
+    o = player2Name;
   } else {
     x = player2Name;
+    o = player1Name
   }
-  return x;
+  return x, o;
 };
 
 
 // checks if tile is available
-var isAvailable = function(tile) {
-  if (tile.hasClass('x-layer') || tile.hasClass('o-layer')) {
+var isAvailable = function(index) {
+  if ((board[index] === 'x') || (board[index] === 'o')) {
     return false;
   } else {
     return true;
@@ -25,47 +37,184 @@ var isAvailable = function(tile) {
 
 
 // sets move for player x if tile is available
-var moveX = function($this, x, o) {
-  if (isAvailable($(this)) === false) {
-      displayAlert(x + ', that square is taken. Choose another.');
+var setX = function(index) {
+  if (isAvailable(index) === false) {
+      return false;
     } else {
-      $this.addClass('x-layer');
+      return board[index] = 'x';
+      // $this.addClass('x-layer');
     }
 };
 
 
 // sets move for player o if tile is available
-var moveO = function($this, x, o) {
-  if (isAvailable($(this)) === false) {
-      displayAlert(o + ', that square is taken. Choose another.');
+var setO = function(index) {
+  if (isAvailable(index) === false) {
+      return false;
     } else {
-      $this.addClass('o-layer');
+      return board[index] = 'o';
+      // $this.addClass('o-layer');
     }
 };
 
+// get player x's move
+var getMoveX = function(move) {
+  return move || getInput();
+};
+
+// get player o's move
+var getMoveO = function(move) {
+  return move || getInput();
+};
 
 
-// checks tiles to determine if there's a winner; alerts if cat's game or players should continue
-var checkWinner = function(count, layer, player1, player2) {
-  var winStatus;
-  if (($('.tile0').hasClass(layer) && $('.tile1').hasClass(layer) && $('.tile2').hasClass(layer)) || // 1st row
-      ($('.tile3').hasClass(layer) && $('.tile4').hasClass(layer) && $('.tile5').hasClass(layer)) || // 2nd row
-      ($('.tile6').hasClass(layer) && $('.tile7').hasClass(layer) && $('.tile8').hasClass(layer)) || // 3rd row
-      ($('.tile0').hasClass(layer) && $('.tile3').hasClass(layer) && $('.tile6').hasClass(layer)) || // 1st col
-      ($('.tile1').hasClass(layer) && $('.tile4').hasClass(layer) && $('.tile7').hasClass(layer)) || // 2nd col
-      ($('.tile2').hasClass(layer) && $('.tile5').hasClass(layer) && $('.tile8').hasClass(layer)) || // 3rd col
-      ($('.tile0').hasClass(layer) && $('.tile4').hasClass(layer) && $('.tile8').hasClass(layer)) || // diag L-R
-      ($('.tile2').hasClass(layer) && $('.tile4').hasClass(layer) && $('.tile6').hasClass(layer))) { // diag R-L
-    $('.default-tile').off('click');
-    displayAlert(player1 + ', You WON! Click "Play Again" to play again or "New Game" to restart');
-    winStatus = true;
+// checks tiles to determine if there's a winner or cat's game
+var checkWinner = function(){
+  var winner;
+  if ((checkRow() === 'x' ) || (checkCol() === 'x') || (checkDiag() === 'x')) {
+    winner = 'x';
+    // console.log('winner is x');
+    return winner;
+  } else if ((checkRow() === 'o' ) || (checkCol() === 'o') || (checkDiag() === 'o')) {
+    winner = 'o';
+    // console.log('winner is o');
+    return winner;
   } else if (count === 9) {
-      displayAlert('Sorry '+ player1 + ' & ' + player2 + ', Cat\'s Game! Click "Play Again" to pagain or "New Game" to restart');
-  } else if (count < 9) {
-    displayAlert(player2 + ', pick a square');
-  } return winStatus;
+    winner = 'cats';
+    // console.log('no winner. cat\'s game');
+    return winner;
+  } else {
+    return false;
+  }
+};
+
+
+// checks rows for a winner
+var checkRow = function() {
+  if ((board[0] === board[1] && board[1] === board[2] && board[2] === 'x') ||
+      (board[3] === board[4] && board[4] === board[5] && board[5] === 'x') ||
+      (board[6] === board[7] && board[7] === board[8] && board[8] === 'x')) {
+      return 'x';
+  } else if ((board[0] === board[1] && board[1] === board[2] && board[2] === 'o') ||
+              (board[3] === board[4] && board[4] === board[5] && board[5] === 'o') ||
+              (board[6] === board[7] && board[7] === board[8] && board[8] === 'o')){
+      return 'o';
+  } else {
+    return false;
+  }
+};
+
+// checks columns for a winner
+var checkCol = function() {
+  if ((board[0] === board[3] && board[3] === board[6] && board[6] === 'x') ||
+      (board[1] === board[4] && board[4] === board[7] && board[7] === 'x') ||
+      (board[2] === board[5] && board[5] === board[8] && board[8] === 'x')) {
+    return 'x';
+  } else if ((board[0] === board[3] && board[3] === board[6] && board[6] === 'o') ||
+              (board[1] === board[4] && board[4] === board[7] && board[7] === 'o') ||
+              (board[2] === board[5] && board[5] === board[8] && board[8] === 'o')) {
+    return 'o';
+  } else {
+    return false;
+  }
+};
+
+// checks diagnals for a winner
+var checkDiag = function() {
+  if ((board[0] === board[4] && board[4] === board[8] && board[8] === 'x') ||
+      (board[2] === board[4] && board[4] === board[6] && board[6] === 'x')) {
+    return 'x';
+  } else if ((board[0] === board[4] && board[4] === board[8] && board[8] === 'o') ||
+            (board[2] === board[4] && board[4] === board[6] && board[6] === 'o')) {
+    return 'o';
+  } else {
+    return false;
+  }
 };
 
 
 
+var playGame = function() {
+  console.log('Lets play tic tac toe');
+  while (checkWinner() === false) {
+    if (count === 0 || (count % 2 === 0)) {
+      setX(getMoveX());
+      count++;
+      checkWinner();
+    } else {
+      setO(getMoveO());
+      count++;
+      checkWinner();
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+$('.default-tile').off('click');
+    displayAlert(player1 + ', You WON! Click "Play Again" to play again or "New Game" to restart');
+
+
+ displayAlert('Sorry '+ player1 + ' & ' + player2 + ', Cat\'s Game! Click "Play Again" to pagain or "New Game" to restart');
+
+
+ displayAlert(player2 + ', pick a square');
+
+// ////// manual modual pattern
+
+// function ...return is only thing accessible
+
+// return {
+//   methods
+// }
+
+
+// var glfunc = fucntion() {
+
+
+
+//   reutn {
+//     a: ___,
+//     b: __
+//   }
+// }
+
+// var x = glfunc();
+
+// x.a();
+// x.b();
+
+// var _a = function(){};
+
+// return { a: _a}
+
+
+// var game = (function(){
+//   var _a = (function() {
+
+//   return {
+//     a: _a,
+//     b: _b
+//   };
+// });
+
+  // Immediately Invoked Function Expression (iffy) --define and invoke in one swoop
+// --return is object we want publicly accessible
+// game.a();
+
+
+
+
+// game logic
+//   iswinner
+
+
+// UI
+//   show move
 
