be8e0a8feaaccb56aca8bbb55ae65686a9b61493
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-09 13:54:52 -0400

    Computer function started
    Need to implement a way to add a class to empty tiles

--- activity.js
+++ activity.js
@@ -28,62 +28,70 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // $('#playComp').on('click', function() {
 
 
-//   var compChoices = function() {
-//     if ($('#tile1').hasClass('x-layer') && $('#tile3').hasClass('x-layer')) {
-//       if (isAvailable($('#tile2')) === true) {
-//         $('#tile2').addClass('o-layer');
-//       } else {
-//         find(($('.default-tile')).addClass('o-layer');
-//       }
-
-//     } else if ($('#tile1').hasClass('x-layer') && $('#tile7').hasClass('x-layer')) {
-//       $('#tile4').addClass('o-layer');
-//     } else if ($('#tile1').hasClass('x-layer') && $('#tile9').hasClass('x-layer')) {
-//       $('#tile5').addClass('o-layer');
-//     } else if ($('#tile1').hasClass('x-layer') && $('#tile2').hasClass('x-layer')) {
-//       $('#tile3').addClass('o-layer');
-//     } else if ($('#tile1').hasClass('x-layer') && $('#tile4').hasClass('x-layer')) {
-//       $('#tile7').addClass('o-layer');
-//     } else if ($('#tile2').hasClass('x-layer') && $('#tile3').hasClass('x-layer')) {
-//       $('#tile1').addClass('o-layer');
-//     } else if ($('#tile2').hasClass('x-layer') && $('#tile5').hasClass('x-layer')) {
-//       $('#tile8').addClass('o-layer');
-//     } else if ($('#tile3').hasClass('x-layer') && $('#tile6').hasClass('x-layer')) {
-//       $('#tile9').addClass('o-layer');
-//     } else if ($('#tile3').hasClass('x-layer') && $('#tile9').hasClass('x-layer')) {
-//       $('#tile6').addClass('o-layer');
-//     } else if ($('#tile3').hasClass('x-layer') && $('#tile5').hasClass('x-layer')) {
-//       $('#tile7').addClass('o-layer');
-//     } else if ($('#tile3').hasClass('x-layer') && $('#tile9').hasClass('x-layer')) {
-//       $('#tile5').addClass('o-layer');
-//     } else if ($('#tile4').hasClass('x-layer') && $('#tile5').hasClass('x-layer')) {
-//       $('#tile6').addClass('o-layer');
-//     } else if ($('#tile4').hasClass('x-layer') && $('#tile6').hasClass('x-layer')) {
-//       $('#tile4').addClass('o-layer');
-//     } else if ($('#tile7').hasClass('x-layer') && $('#tile9').hasClass('x-layer')) {
-//       $('#tile8').addClass('o-layer');
-//     } else if ($('#tile7').hasClass('x-layer') && $('#tile8').hasClass('x-layer')) {
-//       $('#tile9').addClass('o-layer');
-//     } else if
-
-
 //   };
+
+
+
+
+
+// var board = ['', '', '', '', '', '', '', '', ''];
+
+// var compMove = function(layer, token) {
+
+//   // find's the empty tile on the board
+//   var indices = [];
+//   var element = '';
+//   var idx = board.indexOf(element);
+//   var emptyTile;
+//   while (idx != -1) {
+//     indices.push(idx);
+//     idx = board.indexOf(element, idx + 1);
+//   }
+
+
+
+//   if ((board[0] === board[1]) || (board[5] === board[8]) || (board[6] === board[4])) {
+//     if (isAvailable($('.tile2')) === false) {
+//       // addClass('layer') to an empty tile
+//     } else {
+//       $('.tile2').addClass('layer');
+//       board[2] = token;
+//     }
+//   } else if ((board[1] === board[2]) || (board[3] === board[6])) {
+//     $('.tile0').addClass('layer');
+//     board[0] = token;
+//   } else if ((board[0] === board[2]) || (board[4] === board[7])) {
+//     $('.tile1').addClass('layer');
+//     board[1] = token;
+//   } else if ((board[3] === board[4]) || (board[2] === board[8])) {
+//     $('.tile5').addClass('layer');
+//     board[5] = token;
+//   } else if ((board[3] === board[5]) || (board[1] === board[7]) || (board[0] === board[8]) || (board[2] === board[6])) {
+//     $('.tile4').addClass('layer');
+//     board[4] = token;
+//   } else if ((board[4] === board[5]) || (board[0] === board[6])) {
+//     $('.tile3').addClass('layer');
+//     board[3] = token;
+//   } else if ((board[6] === board[7]) || (board[0] === board[4]) || (board[2] === board[5])) {
+//     $('.tile8').addClass('layer');
+//     board[8] = token;
+//   } else if ((board[6] === board[8]) || (board[1] === board[4])) {
+//     $('.tile7').addClass('layer');
+//     board[7] = token;
+//   } else if ((board[7] === board[8]) || (board[2] === board[4])) {
+//     $('.tile6').addClass('layer');
+//     board[6] = token;
+//   } else if (board[0] === board[3]) {
+//     $('.tile6').addClass('layer');
+
+
+// };
+
+
+
 
 
 
